e600a482f008cadacf68e3c357cea1ff26ba8fbe
KKardd <wlstjd4013@gmail.com>
2025-02-25 08:03:44 +0900

    feat: separate register-user constant

---
+++ src/common/registered-user.ts
@@ -0,0 +1,11 @@
+export const registeredUsers = {
+    "jun020216@sookmyung.ac.kr": "U07C0N85M2P", // 예준님
+    "itoodo12@gmail.com": "U07BY0KMU69", // 수민님
+    "maroony55@gmail.com": "U07BU0BSSK0", // 가영님
+    "gurwns9325@cau.ac.kr": "U07C32AK8KE", // 혁준님
+    "su10jin11@khu.ac.kr": "U07C19GL9V1", // 수진님
+    "wls4013@inu.ac.kr": "U07BL68MR8F", // 진성님
+    "hjforaws@gmail.com": "U07CDBSB2BB", // 현제님
+    "hajuny129@hufs.ac.kr": "U05FAS0GB99", // 하준님
+    "kdhhuns2000@gmail.com": "U07C1RDLFQS", // 도훈님
+};
