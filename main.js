f6aedfb50462ae689e182ee478b07c275da571f8
KKardd <108740187+KKardd@users.noreply.github.com>
2025-04-17 23:31:15 +0900

    혁준이는 일주일동안 쉰대요.

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const registeredUsers = {
     "jun020216@sookmyung.ac.kr": "U07C0N85M2P", // 예준님
     "itoodo12@gmail.com": "U07BY0KMU69", // 수민님
     "maroony55@gmail.com": "U07BU0BSSK0", // 가영님
-    "gurwns9325@cau.ac.kr": "U07C32AK8KE", // 혁준님
+    // "gurwns9325@cau.ac.kr": "U07C32AK8KE", // 혁준님
     "su10jin11@khu.ac.kr": "U07C19GL9V1", // 수진님
     "wls4013@inu.ac.kr": "U07BL68MR8F", // 진성님
     "hjforaws@gmail.com": "U07CDBSB2BB", // 현제님
