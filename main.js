1dd786dacb2c7783392fe1da650a51e371a4c031
KKardd <108740187+KKardd@users.noreply.github.com>
2025-05-12 09:02:11 +0900

    수진님 일본 여행

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const registeredUsers = {
     "itoodo12@gmail.com": "U07BY0KMU69", // 수민님
     "maroony55@gmail.com": "U07BU0BSSK0", // 가영님
     "gurwns9325@cau.ac.kr": "U07C32AK8KE", // 혁준님
-    "su10jin11@khu.ac.kr": "U07C19GL9V1", // 수진님
+    // "su10jin11@khu.ac.kr": "U07C19GL9V1", // 수진님
     "wls4013@inu.ac.kr": "U07BL68MR8F", // 진성님
     "hjforaws@gmail.com": "U07CDBSB2BB", // 현제님
     // "hajuny129@hufs.ac.kr": "U05FAS0GB99", // 하준님
