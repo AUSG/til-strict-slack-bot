ede603be2199763a860b1eaa8160cb29af08554d
KKardd <108740187+KKardd@users.noreply.github.com>
2026-01-20 09:09:40 +0900

    현제띠 복귀🔥

--- main.ts
+++ main.ts
@@ -19,7 +19,7 @@ const registeredUsers = {
     "gurwns9325@cau.ac.kr": "U07C32AK8KE", // 혁준님
     "su10jin11@khu.ac.kr": "U07C19GL9V1", // 수진님
     "wls4013@inu.ac.kr": "U07BL68MR8F", // 진성님
-    // "hjforaws@gmail.com": "U07CDBSB2BB", // 현제님
+    "hjforaws@gmail.com": "U07CDBSB2BB", // 현제님
     // "hajuny129@hufs.ac.kr": "U05FAS0GB99", // 하준님
     "kdhhuns2000@gmail.com": "U07C1RDLFQS", // 도훈님
 };
